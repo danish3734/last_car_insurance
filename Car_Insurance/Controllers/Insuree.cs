299ae719e63b82ad5ef967b62fcde95ddcea63b9
danish748 <143021245+danish3734@users.noreply.github.com>
2024-03-15 13:29:19 -0400

    This is last commit of change

---
+++ Car_Insurance/Controllers/Insuree.cs
@@ -0,0 +1,13 @@
+﻿
+
+
+    public class Insuree
+    {
+        public int Age { get; internal set; }
+        public int CarYear { get; internal set; }
+        public string CarMake { get; internal set; }
+        public string CarModel { get; internal set; }
+        public int SpeedingTickets { get; internal set; }
+        public bool HasDUI { get; internal set; }
+        public bool IsFullCoverage { get; internal set; }
+    }
